b12aa54c57f634c029a3ba26249e31b245731d47
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-10-04 13:21:20 +0200

    add readme to make vue.js hello World work

--- feweb_project/src/App.vue
+++ feweb_project/src/App.vue
@@ -1,26 +1,17 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { reactive } from 'vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const counter = reactive({
+  count: 0
+})
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+console.log(counter.count) // 0
+counter.count++
+
+// component logic
+// declare some reactive state here.
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>Hallo World!</h1>
+</template>
