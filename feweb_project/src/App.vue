28e97a4b7f73d8dca771bbb3506be7160aee4956
wi21b095 <wi21b095@technikum-wien.at>
2023-11-10 18:49:06 +0100

    router eingefügt

--- feweb_project/src/App.vue
+++ feweb_project/src/App.vue
@@ -13,5 +13,10 @@ counter.count++
 </script>
 
 <template>
-  <h1>Hallo World!</h1>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link>
+  </nav>
+  <router-view />
 </template>
---
+++ feweb_project/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/posts/:id',
+    name: 'post',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+  },
+  /*{
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ /*'../views/AboutView.vue')
+  }*/
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
