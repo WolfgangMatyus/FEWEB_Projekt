2cf52b6f18737ff839d97f93959e3bd800164e2a
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-11-15 13:21:16 +0100

    favicon fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,12 +34,12 @@ export default {
       subtitleContent: 'Hier sehen Sie unsere neuesten Objekte:',
       ClassStyle: ' col-sm-7 col-md-5 col-lg-3',
       cards: [
-        { id: 1, title: "Auktionsobjekt 1", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/src/assets/img/SYS_logo.jpg", content: "Content 1" },
-        { id: 2, title: "Auktionsobjekt 2", subtitle: "Dieses Objekt ist einzigartig...", imageUrl:"/img/logo.png", content: "Content 2" },
-        { id: 3, title: "Auktionsobjekt 3", subtitle: "Dieses Objekt ist speziell für...", imageUrl:"/img/logo.png", content: "Content 3" },
-        { id: 4, title: "Auktionsobjekt 4", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/logo.png", content: "Content 4" },
-        { id: 5, title: "Auktionsobjekt 5", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/logo.png", content: "Content 5" },
-        { id: 6, title: "Auktionsobjekt 6", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/logo.png", content: "Content 6" },
+        { id: 1, title: "Auktionsobjekt 1", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/SYS_logo.jpg", content: "Content 1" },
+        { id: 2, title: "Auktionsobjekt 2", subtitle: "Dieses Objekt ist einzigartig...", imageUrl:"/img/SYS_logo.jpg", content: "Content 2" },
+        { id: 3, title: "Auktionsobjekt 3", subtitle: "Dieses Objekt ist speziell für...", imageUrl:"/img/SYS_logo.jpg", content: "Content 3" },
+        { id: 4, title: "Auktionsobjekt 4", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/SYS_logo.jpg", content: "Content 4" },
+        { id: 5, title: "Auktionsobjekt 5", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/SYS_logo.jpg", content: "Content 5" },
+        { id: 6, title: "Auktionsobjekt 6", subtitle: "Dieses Objekt ist ganz was besonderes...", imageUrl:"/img/SYS_logo.jpg", content: "Content 6" },
       ],
     };
   },
